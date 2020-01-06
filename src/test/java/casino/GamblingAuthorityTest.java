1065d399b90398e2c677405e2c780647228e94b7
User <r.simkhada@student.fontys.nl>
2020-01-06 19:48:47 +0100

    GamblingAuthority tests are added.

--- src/test/java/casino/GamblingAuthorityTest.java
+++ src/test/java/casino/GamblingAuthorityTest.java
@@ -1,7 +1,36 @@
 package casino;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GamblingAuthorityTest {
+    /**
+     * This test method check if it returns the token string.
+     */
+    @Test
+    public void ShouldReturnTokenString() {
+        //arrange
+        //act
+        //assert
+    }
 
+    /**
+     * This method should check if addBet() method adds bet object to the list.
+     */
+    @Test
+    public void ShouldAddBetToTheList() {
+        //arrange
+        //act
+        //assert
+    }
+    /**
+     * This method should check if getWinNo() method returns the number.
+     */
+    @Test
+    public void ShouldReturnWinNumber() {
+        //arrange
+        //act
+        //assert
+    }
 }
